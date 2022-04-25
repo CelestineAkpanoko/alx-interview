579e695422eed6542ee790df766eb46d46190a66
Celestine Akpanoko <celestineakpanoko@gmail.com>
2022-04-25 02:51:50 +0100

    determines if all the boxes can be opened.

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''A python script that solves the lockboxes problem.
+'''
+
+
+def canUnlockAll(boxes):
+    '''determines if all the boxes in n number of locked boxes where
+    Each box is numbered sequentially from 0 to n - 1 and
+    each box may contain keys to the other boxes.
+    '''
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
